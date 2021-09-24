4c5d9ffc839679274110918d0416736b182536d1
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2021-09-24 20:42:09 +0200

    Update views.py

--- views.py
+++ views.py
@@ -5,7 +5,7 @@ from starlette.requests import Request
 from werkzeug.security import generate_password_hash
 from utilities import DowDogs, User
 
-
+# Pointing to correct folder for html templates
 templates = Jinja2Templates('templates')
 
 
@@ -28,19 +28,25 @@ def index_get(request: Request):
 @router.post("/")
 def index_post(request: Request, email: str = Form(...), password: str = Form(...)):
 
-
+    # Checking for existing email address
     if email not in dow_user.users['email']:
 
         try:
-
+            
+            # Generating password hash 
             pass_hash = generate_password_hash(password)
+            
+            # Saving user credentials
             dow_user.users['pass_hash'].append(pass_hash)
             dow_user.users['email'].append(email)
+            
             return templates.TemplateResponse('index.html', context={'request': request, 'pass_hash': pass_hash, 'email': email.upper()})
 
         except KeyError:
             msg = 'WROND PASSWORD!'
             return templates.TemplateResponse('index.html', context={'request': request, 'msg': msg})
+        
+    # Flashing error message when encountering email address duplication
     else:
         msg = 'SORRY, THAT EMAIL ALREADY EXISTS IN OUR DATABASE!'
         return templates.TemplateResponse('index.html', context={'request': request, 'msg': msg})
@@ -51,6 +57,8 @@ def index_post(request: Request, email: str = Form(...), password: str = Form(..
 @router.get("/signals")
 def read_current_user(request: Request, username: str = Depends(dow_user.get_current_username)):
     msg = 'CONNECTION ERROR. PLEASE TRY AGAIN LATER.'
+    
+    # Checking for correct number of results
     try:
         return templates.TemplateResponse('signals.html', context={'request': request, 'trend': trend})
     except:
