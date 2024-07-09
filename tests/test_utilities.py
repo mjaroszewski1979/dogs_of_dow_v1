b9d138ef2d205354bf0e0ae4a5a396d5df70801f
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2024-07-09 10:37:21 +0200

    Update test_utilities.py

--- tests/test_utilities.py
+++ tests/test_utilities.py
@@ -1,10 +1,13 @@
-from utilities import DowDogs, User
+# Import pytest for writing and running tests
 import pytest
+# Import DowDogs and User classes from the utilities module
+from utilities import DowDogs, User
 
+# Create instances of DowDogs and User for testing
 dow_dog = DowDogs()
 dow_user = User()
 
-# Ensures that attributes of DowDogs class exists
+
 @pytest.mark.parametrize("dow_dog_attributes, unexpected_result", [
     (dow_dog.start, None),
     (dow_dog.end, None),
@@ -14,21 +17,34 @@ dow_user = User()
     (dow_dog.tickers, None)
 ])
 def test_dow_dog_attributes(dow_dog_attributes, unexpected_result):
+    """
+    Test to ensure that attributes of the DowDogs class exist.
+    This verifies that specific attributes are not None.
+    """
     assert dow_dog_attributes != unexpected_result
 
-# Ensures that get trend method works correctly
 def test_get_trend():
+    """
+    Test the get_trend method of the DowDogs class.
+    This method checks if the get_trend method returns the expected data.
+    """
     data = dow_dog.get_trend()
     result = ['SELL', '3M']
     assert result in data
 
-# Ensures that get data method works correctly
 def test_get_data():
+    """
+    Test the get_data method of the DowDogs class.
+    This method checks if the get_data method works correctly and updates the results attribute.
+    """
     dow_dog.get_data('MMM')
     result = ['SELL', '3M']
     assert result in dow_dog.results
 
-# Ensures that attribute of User class exists
 def test_dow_user():
+    """
+    Test to ensure that the users attribute of the User class exists.
+    This verifies that the users attribute is not None.
+    """
     assert dow_user.users != None
 
